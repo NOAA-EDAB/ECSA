e16b1ae5af8670821fc7295ed10271830667579d
Sean Hardison <seanhardison@gmail.com>
2018-08-23 16:13:15 -0400

    get svspp

--- R/template_control.R
+++ R/template_control.R
@@ -4,6 +4,8 @@ create_template <- function(survdat_name = "SUMMER FLOUNDER",
   library(dplyr)
   
   load("data/df_survdat.rda")
+  spp <- read.csv("data/species_list.csv")
+  svspp <- unique(as.numeric(spp[spp$com_name == survdat_name,]$svspp))
   
   firstup <- function(x) {
     substr(x, 1, 1) <- toupper(substr(x, 1, 1))
@@ -12,7 +14,7 @@ create_template <- function(survdat_name = "SUMMER FLOUNDER",
   
   clean_names <- df_survdat %>%
     filter(COMNAME == survdat_name) %>% 
-    select(SCINAME, COMNAME) %>% 
+    dplyr::select(SCINAME, COMNAME) %>% 
     distinct() %>% 
     mutate(common_name = tolower(COMNAME),
            sci_name = firstup(tolower(SCINAME)),
@@ -31,12 +33,11 @@ create_template <- function(survdat_name = "SUMMER FLOUNDER",
                   -COMNAME)
   
   dat <- readLines("generic_template.rmd")
-  
   dat <- gsub("\\{\\{COMMON_NAME\\}\\}", clean_names$common_name, dat)
   dat <- gsub("\\{\\{SCI_NAME\\}\\}", clean_names$sci_name, dat)
   dat <- gsub("\\{\\{CC_NAME\\}\\}", clean_names$cc_name, dat)
   dat <- gsub("\\{\\{SPECIES_CODE\\}\\}", clean_names$species_code, dat)  
-  
+  dat <- gsub("\\{\\{svspp\\}\\}", svspp, dat)  
   file_name <- sprintf("ECSA_%s.rmd", clean_names$cc_name)
 
   if(file.exists(file_name) &
