0c75c295654a84b37c936a50d468cb2b96ddaa87
seanhardison1 <seanhardison@gmail.com>
2019-06-11 18:58:50 -0400

    Added STARS changepoint analysis. Added color-blind friendly colors. Fixed plotting.

--- R/tab_plotly.R
+++ R/tab_plotly.R
@@ -1,4 +1,4 @@
-tab_plotly <- function(df, showlegend = T){
+tab_plotly <- function(df, showlegend = T, series.name = NULL){
   
   if (showlegend) {
     p <- plot_ly(type = 'scatter', mode = 'lines', showlegend = T)
@@ -8,12 +8,31 @@ tab_plotly <- function(df, showlegend = T){
   
   plotvars <- names(df)[2:ncol(df)]
   for (i in 1:length(plotvars)){
+    
+    if (plotvars[i] == "Regime.Mean" ) {
+      color <- "#b2df8a"
+    } else if (plotvars[i] == "Trend"| plotvars[i] == "Fall"){
+      color <- "#a6cee3"
+    } else if (plotvars[i] == "Series"| plotvars[i] == "Spring") {
+      if (!is.null(series.name)){
+        colnames(df)[colnames(df) == "Series"] <- series.name
+        plotvars[i] <- series.name
+        }
+      color <- "#1f78b4"
+    } 
+    
     plot_df <- df %>%
       rename(data=one_of(plotvars[i])) %>%
       dplyr::select(Time, data)
     
-    p <- p %>%
-      add_lines(data=plot_df, x=~Time, y=~data, name=plotvars[i]) 
+    if (length(plotvars) <= 3) {
+      p <- p %>%
+        add_lines(data=plot_df, x=~Time, y=~data, name=plotvars[i], line = list(color = color)) 
+    } else {
+      p <- p %>%
+        add_lines(data=plot_df, x=~Time, y=~data, name=plotvars[i]) 
+    }
+    
   }
   
 
