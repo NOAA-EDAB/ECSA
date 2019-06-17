b7e94c09f1e4a1a3818e87629340c9e91c73cb5c
andybeet <22455149+andybeet@users.noreply.github.com>
2019-06-17 17:13:31 -0400

    rename file to create_template.R

--- R/create_template.R
+++ R/create_template.R
@@ -1,39 +1,35 @@
+#'#' Create an ECSA bookdown template for a species of interest. 
 #'
-#' A function to create an ECSA bookdown template for a species of interest. 
 #'
 #'
-#' @param survdat_name A common species name as defined by survdat. Options as of 8/24 include 
-#' JONAH CRAB, AMERICAN LOBSTER, and SUMMER FLOUNDER.
-#' @param output_dir The output directory for the compiled bookdown HTML document and supporting files.
-#' @param book_filename The filename of the compiled bookdown HTML document. Defaults "to ECSA_\code{survdat_name}.html".
-#' @param render_book Logical. If TRUE, the generated template will render a bookdown HTML document into the chosen directory.
-#' Otherwise, a template .Rmd file will be generated in the ECSA package directory.   
+#' @param stock_name A common species name as defined by ??? (eg. atlantic-menhaden, smooth-dogfish)
+#' @param output_dir The output directory for the compiled bookdown HTML document and supporting files. Default "docs" folder in current working directory
+#' @param render_book Logical. If TRUE, the generated template will render a bookdown HTML document into the chosen directory and open in a web browser.
+#' If FALSE, the template .Rmd file will be generated in the directory output_dir/stock-name folder   
 #' @param overwrite Logical. If TRUE, output will overwrite any existing template for chosen species.
-#' @param make_interactive Logical. If TRUE, template time series will be made interative through the plotly library.
 #'
-#' @return A .Rmd file populated with figures that can be knit into an ECSA report skeleton. 
+#'
+#'
+#' @return A .Rmd file and/or bookdown html document populated with figures
 #'
 #' @importFrom magrittr "%>%"
 #' 
-#' @examples  
-#'
-#' create_template(survdat_name = "SMOOTH DOGFISH", overwrite = T, output_dir = getwd())
+#' @examples
+#' 
+#' create_template(stock_name = "smooth-dogfish", overwrite = T, output_dir = getwd(), render_book = T)
+#' 
 #' @export
 
 
-create_template <- function(stock_name,
-                            # stock_code,
-                            # sci_name,
-                            overwrite = FALSE,
-                            make_interactive = FALSE,
-                            # output_dir = "/docs",
-                            book_filename = NULL,
-                            render_book = F){
+create_template <- function(stock_name, 
+                            output_dir = here::here("docs"),
+                            render_book = T,
+                            overwrite = F) {
   
   `%>%` <- magrittr::`%>%`
   library(readr)
   
-  setwd(here::here())
+  #setwd(here::here())
   ## Select the stock and format stock area and common name
   clean_names <- readr::read_csv(here::here("data","stock_data/stock_list.csv"),
                                  col_types = readr::cols(
@@ -87,94 +83,56 @@ create_template <- function(stock_name,
 
   # cat(dat,sep = "\n" )
   file_name <- sprintf("ECSA_%s.rmd", clean_names$stock_name)
-  folder_name <- sprintf("docs/%s", clean_names$stock_name)
-  output_dir <- sprintf("%s_book", clean_names$stock_name)
+  folder_name <- sprintf("%s/%s",output_dir, clean_names$stock_name)
+  #output_dir <- sprintf("%s_book", clean_names$stock_name)
   
+  # create the output directory if missing
   if(!dir.exists(folder_name)) {
-    dir.create(folder_name)
+    dir.create(folder_name,recursive = T)
   }
 
   #Adjust _bookdown.yml accordingly
   bookyml <- suppressWarnings(
     readLines(here::here("templates","_bookdown_template.yml"))
   )
-  bookyml <- stringr::str_replace(bookyml,
-                                  "\\[.*\\]",
-                                  sprintf('["ECSA_%s.rmd"]',
-                                          clean_names$stock_name))
-  # cat(bookyml, sep = "\n")
-  
+  # replace .rmd file name
+  bookyml <- stringr::str_replace(bookyml, "\\[.*\\]", sprintf('["ECSA_%s.rmd"]', clean_names$stock_name))
   #Set output directory for bookdown files
-  # if(!is.null(output_dir)){
-  #   bookyml <- stringr::str_replace(bookyml,
-  #                                   'output_dir: ".*"',
-  #                                   sprintf('output_dir: "%s"',
-  #                                           output_dir))
-  # } else {
-  bookyml <- stringr::str_replace(bookyml,
-                                  'output_dir: .*',
-                                  sprintf('output_dir: %s',
-                                          output_dir))
-  # }
-  
+  bookyml <- stringr::str_replace(bookyml, 'output_dir: .*', sprintf('output_dir: %s', output_dir))
   #Set filename of final HTML document (by default this is the title of the template)
-  # if (!is.null(book_filename)) {
-    # bookyml <- stringr::str_replace(bookyml,
-                                    # 'book_filename: ".*"',
-                                    # sprintf('book_filename: "%s"',
-                                            # book_filename))
-  # } else {
-    bookyml <- stringr::str_replace(bookyml,
-                                    'book_filename: ".*"',
-                                    sprintf('book_filename: "ECSA_%s_working_draft"',
-                                            clean_names$stock_name))
-  # }
-  
+  bookyml <- stringr::str_replace(bookyml, 'book_filename: ".*"', 
+                                sprintf('book_filename: "ECSA_%s_working_draft"', clean_names$stock_name))
+
   #Check to make sure existing file is not over-written
-  if(file.exists(file_name) &
-     !overwrite){
-    stop(sprintf("\nEasy, Cowboy!\n%s already exists. If you want to do this, change 'overwrite = TRUE'",
-                 file_name))
+  if(file.exists(sprintf("%s/%s",folder_name,file_name)) &  !overwrite){
+    stop(sprintf("\nEasy, Cowboy!\n%s already exists. If you want to do this, change 'overwrite = TRUE'", file_name))
   }
   
-  file_connection <- file(sprintf("%s/%s", 
-                                  folder_name, 
-                                  file_name))
+  # writes generic template after species specific substitutions to .rmd
+  file_connection <- file(sprintf("%s/%s", folder_name, file_name))
   writeLines(dat, file_connection)
   close(file_connection)
+  
   message(sprintf("ECSA template written to %s",
                   sprintf("%s/%s", folder_name, file_name)))
   
-  book_connection <- file(sprintf("%s/_bookdown.yml",
-                                  folder_name),
-                          open = "w")
+  # copy _bookdown.yml to differnt location
+  book_connection <- file(sprintf("%s/_bookdown.yml", folder_name), open = "w")
   writeLines(bookyml, book_connection)
   close(book_connection)
-  message(sprintf("\n%s/_bookdown.yml successfully created.",
-                  folder_name))
+  
+  message(sprintf("\n%s/_bookdown.yml successfully created.", folder_name))
 
 
   # render the species specific markdown file into book
   if (render_book){
-    pathToDir <- here::here(sprintf("%s", folder_name))
-    pathToRmd <- here::here(sprintf("%s/%s", folder_name,file_name))
+    pathToDir <- sprintf("%s", folder_name)
+    pathToRmd <- sprintf("%s/%s", folder_name,file_name)
     #bookdown::render_book(sprintf("%s/%s",folder_name,file_name))
     rmarkdown::render(pathToRmd)
     browseURL(paste0(pathToDir,"/overview.html"))
   }
-  if(file.exists(sprintf("%s/ECSA_%s.html", 
-                         folder_name,
-                         clean_names$stock_name))) {
-    
-  }
   
 
-  #setwd(pathToRmd) # is this needed?
-
 }
-# create_template(survdat_name = "ATLANTIC-MENHADEN",
-#                 stock_code = "atlmen",
-#                 sci_name  = "Brevoortia tyrannus",
-#                   output_dir = "~/bookdown_output", #Creates new folder by default
-#                 overwrite = T)
-# bookdown::render_book("ECSA_ATLANTIC-MENHADEN.rmd")
+
