6326b40bc4a77edbdb1cddc0c3db8cefe7aa3624
seanhardison1 <seanhardison@gmail.com>
2019-07-10 16:05:18 -0400

    Occupancy data and processing script

--- data-raw/collapse_into_stack.R
+++ data-raw/collapse_into_stack.R
@@ -8,35 +8,32 @@ library(dplyr)
 #The argument fname_regex allows for filtering file names according to some regex. fname_regex passes
 #all files by default (i.e. .*). 
 collapse_into_stack <- function(folder_path, fname_regex = ".*"){
-  p <- folder_path
-  message(p)  
   
+  message(folder_path)  
+  
+  #Select files that meet the relevant regex criteria
+  p <- list.files(folder_path)[stringr::str_detect(list.files(folder_path), fname_regex)]
   
   #Load each files in the directory and put it into a RasterStack
   out <- raster::stack()
-  for (i in 1:length(list.files(p))){
+  for (i in 1:length(p)){
 
-    f <- list.files(p)[i]
+    f <- p[i]
     
-    if (stringr::str_detect(f, "\\.RData|\\.rdata|\\.Rdata") & stringr::str_detect(f, fname_regex)){
+    if (stringr::str_detect(f, "\\.RData|\\.rdata|\\.Rdata")){
       
       fname <- stringr::str_split(f, "\\.RData|\\.rdata|\\.Rdata")[[1]][1]
-      obj <- loadRData(file.path(p,f))
+      obj <- loadRData(file.path(folder_path, f))
       
       
       assign("out",stack(out,obj))
       names(out)[i] <- fname
       message(fname)
-    } else if (!stringr::str_detect(f, "\\.RData|\\.rdata|\\.Rdata")) {
+    } else {
       message(paste0(f, " is not a .RData file"))
-      next
-    } else if (!stringr::str_detect(f, fname_regex)){
-      message(paste0(f, "does not meet regex criteria."))
-      next
+
     }
-  
   }
-  
   
   return(out)
 }
@@ -50,15 +47,11 @@ loadRData <- function(fileName){
 
 
 
-biomass_fall <- collapse_into_stack(folder_path = file.path(raw.dir,"biomass/fall"))
-biomass_spring <- collapse_into_stack(folder_path = file.path(raw.dir,"biomass/spring"))
-# save(biomass_fall, file = paste0(raw.dir,"/biomass_fall.rdata"))
-# save(biomass_spring, file = paste0(raw.dir,"/biomass_spring.rdata"))
+#NOTE: raw.dir must be the folder containing individual raster files
 
+#Chlorophyll
 chlorophyll_conc <- collapse_into_stack(folder_path = file.path(raw.dir,"chlorophyll concentration"))
-# save(chlorophyll_conc, file = paste0(raw.dir,"/chlorophyll_conc.rdata"))
 
-raw.dir <- "c:/users/sean.hardison/downloads/ecsa menhaden"
 
 #Zooplankton
 for (k in c("spring","fall")){
@@ -101,18 +94,34 @@ for (k in c("bot_sal","surf_sal")){
 }
 
 #Occurrence probability
-raw.dir <- "~"
-  collapse_into_stack(file.path(raw.dir, "spring-occupancy/spring"),
+spring_blabas_occ_prob <- collapse_into_stack(file.path(raw.dir, "spring-occupancy/spring"),
                     fname_regex = "(?=.*blabas)(?=.*PA)")
+save(spring_blabas_occ_prob, file = "spring_blabas_occ_prob.rdata")
 
-unique_species <- 
-  unique(str_extract(list.files(file.path(raw.dir, "spring-occupancy/spring")), "_.*_"))
+spring_monkfh_occ_prob <- collapse_into_stack(file.path(raw.dir, "spring-occupancy/spring"),
+                                              fname_regex = "(?=.*monkfh)(?=.*PA)")
+save(spring_monkfh_occ_prob, file = "spring_monkfh_occ_prob.rdata")
 
+spring_scupzz_occ_prob <- collapse_into_stack(file.path(raw.dir, "spring-occupancy/spring"),
+                                              fname_regex = "(?=.*scupzz)(?=.*PA)")
+save(spring_scupzz_occ_prob, file = "spring_scupzz_occ_prob.rdata")
 
+spring_bluefi_occ_prob <- collapse_into_stack(file.path(raw.dir, "spring-occupancy/spring"),
+                                              fname_regex = "(?=.*bluefi)(?=.*PA)")
+save(spring_bluefi_occ_prob, file = "spring_bluefi_occ_prob.rdata")
 
-list.files(file.path(raw.dir, "spring-occupancy/spring"))
+fall_blabas_occ_prob <- collapse_into_stack(file.path(raw.dir, "fall-occupancy/fall"),
+                                              fname_regex = "(?=.*blabas)(?=.*PA)")
+save(fall_blabas_occ_prob, file = "fall_blabas_occ_prob.rdata")
 
-for (i in 1:length(list.files(file.path(raw.dir, "spring-occupancy/spring")))){
-  
-}
+fall_monkfh_occ_prob <- collapse_into_stack(file.path(raw.dir, "fall-occupancy/fall"),
+                                              fname_regex = "(?=.*monkfh)(?=.*PA)")
+save(fall_monkfh_occ_prob, file = "fall_monkfh_occ_prob.rdata")
 
+fall_scupzz_occ_prob <- collapse_into_stack(file.path(raw.dir, "fall-occupancy/fall"),
+                                              fname_regex = "(?=.*scupzz)(?=.*PA)")
+save(fall_scupzz_occ_prob, file = "fall_scupzz_occ_prob.rdata")
+
+fall_bluefi_occ_prob <- collapse_into_stack(file.path(raw.dir, "fall-occupancy/fall"),
+                                              fname_regex = "(?=.*bluefi)(?=.*PA)")
+save(fall_bluefi_occ_prob, file = "fall_bluefi_occ_prob.rdata")
