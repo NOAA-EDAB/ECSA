225f7e51ca7860ee7208ee1783c4370be0769075
slarge <largesi@gmail.com>
2019-07-10 14:45:58 -0400

    move data aggregation step to data-raw

--- R/create_template.R
+++ R/create_template.R
@@ -30,43 +30,9 @@ create_template <- function(stock_name,
   library(readr)
 
   ## Select the stock and format stock area and common name
-  clean_names <- readr::read_csv(here::here("data","stock_data/stock_list.csv"),
-                                 col_types = readr::cols(
-                                   common_name = readr::col_character(),
-                                   sci_name = readr::col_character(),
-                                   cc_name = readr::col_character(),
-                                   stock_name = readr::col_character(),
-                                   species_code = readr::col_character(),
-                                   svspp = readr::col_double(),
-                                   stock_season = readr::col_character(),
-                                   strata = readr::col_double()
-                                 )) %>%
-    dplyr::filter(stock_name == !!stock_name) %>%
-    dplyr::mutate(stock_subarea = dplyr::case_when(grepl("_sne-ma$", stock_name) ~ "southern New England/mid-Atlantic ",
-                                                 grepl("_cc$", stock_name) ~ "Cape Cod ",
-                                                grepl("_sne$", stock_name) ~ "southern New England ",
-                                                grepl("_gom-ngb$", stock_name) ~ "Gulf of Maine/northern Georges Bank ",
-                                                grepl("_sgb-ma$", stock_name) ~ "southern Georges Bank/mid-Atlantic ",
-                                                grepl("_gb$", stock_name) ~ "Georges Bank ",
-                                                grepl("_gom-ngb$", stock_name) ~ "Gulf of Maine/northern Georges Bank ",
-                                                grepl("_gom$", stock_name) ~ "Gulf of Maine ",
-                                                grepl("_gom-gb$", stock_name) ~ "Gulf of Maine/Georges Bank ",
-                                                grepl("_north$", stock_name) ~ "north ",
-                                                grepl("_south$", stock_name) ~ "south ",
-                                                 TRUE ~ ""),
-                  common_name = gsub("^atlantic", "Atlantic", common_name),
-                  common_name = gsub("^american", "American", common_name),
-                  common_name = gsub("^acadian", "Acadian", common_name)) %>% 
-    dplyr::select(common_name,
-           stock_name,
-           stock_season,
-           stock_subarea,
-           sci_name,
-           cc_name,
-           svspp) %>% 
-    dplyr::select(-stock_season) %>% 
-    dplyr::distinct(.keep_all = TRUE)
-    
+  clean_names <- readr::read_csv(here::here("data-raw","clean_names.csv")) %>%
+    dplyr::filter(stock_name == !!stock_name) 
+  
   if(length(clean_names) < 1){
     stop(sprintf("%s is not found. Check spelling or add %s as a new stock to '%s'", stock_name, stock_name, path.expand("data/stock_data/stock_list.csv")))
   }
@@ -81,7 +47,7 @@ create_template <- function(stock_name,
   dat <- gsub("\\{\\{SPECIES_CODE\\}\\}", clean_names$svspp, dat)  
 
   # cat(dat,sep = "\n" )
-  file_name <- sprintf("%s.rmd", clean_names$stock_name)
+  file_name <- sprintf("%s_draft.rmd", clean_names$stock_name)
   folder_name <- sprintf("%s",output_dir)
   #output_dir <- sprintf("%s_book", clean_names$stock_name)
   
@@ -91,7 +57,7 @@ create_template <- function(stock_name,
   }
 
   #Check to make sure existing file is not over-written
-  if(file.exists(sprintf("%s/%s",folder_name,file_name)) &  !overwrite){
+  if(file.exists(sprintf("%s/%s",folder_name, file_name)) &  !overwrite){
     stop(sprintf("\nEasy, Cowboy!\n%s already exists. If you want to do this, change 'overwrite = TRUE'", file_name))
   } else {
     
